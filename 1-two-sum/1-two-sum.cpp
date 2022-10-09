c22c18e29b44bdc46b62a53bdd665a043005fcfa
ShinysArc <stephane.gelibert@epita.fr>
2022-10-10 00:22:02 +0200

    Time: 408 ms (38.23%), Space: 10.3 MB (57.82%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
-        vector<int> res(2);
-        for (unsigned long i = 0; i < nums.size() - 1; i++)
+        for (int i = 0; i < nums.size() - 1; i++)
         {
-            for (auto j = i + 1; j < nums.size(); j++)
+            for (int j = i + 1; j < nums.size(); j++)
             {
                 if ((nums[i] + nums[j]) == target)
-                {
-                    res[0] = i;
-                    res[1] = j;
-                    return res;
-                }
+                    return { i, j };
             }
         }
         
-        return res;
+        return { 0, 0 };
     }
 };
